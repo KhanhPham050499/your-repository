123713605ce3f60a8772408528743d11238a71f4
KhanhPham050499 <phamngocbaokhanh2015@gmail.com>
2020-08-22 11:01:07 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/your-repository/precache-manifest.d0987c589f62f203d5c5b9d711923ac9.js"
+  "/your-repository/precache-manifest.9339833adf721859dba29f909fb566cf.js"
 );
 
 self.addEventListener('message', (event) => {
